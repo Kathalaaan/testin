c7b6e62565e2fc633db66c24baea604ad532685a
Kathalaaan <nitishhotmail333@gmail.com>
2025-10-26 22:21:56 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/testin/',  // <-- add this line (your repo name)
-  plugins: [react()],
-})
